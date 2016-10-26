6e1b463946d7e1e18b3984e4fb4ec7ef165ca3b4
Alberto <albert@studionone.com.au>
2016-10-26 14:50:14 +1000

    Basic html file.

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alberto
- * Date: 26/10/16
- * Time: 2:25 PM
- */
+
+$noCache = '?timestamp=' . date('U');
+
+?><html lang="en">
+<head>
+    <title>Timbre</title>
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+    <link rel="shortcut icon" href="img/icon-web.png">
+    <link rel="icon" type="image/png" href="img/icon-web.png">
+    <link rel="apple-touch-icon-precomposed" href="img/icon.png">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="styles.css<?php echo $noCache; ?>">
+
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="js/timbre.js<?php echo $noCache; ?>"></script>
+</head>
+<body>
+
+<h1>Timbre</h1>
+
+</body>
+</html>
